86a344ec211ba73cf8a6f057c788baf34712033f
akkoaya <151345394+akkoaya@users.noreply.github.com>
2026-08-22 15:14:37 +0800

    fix: include real-person subjects in keyword search

--- lib/request/apis/bangumi_api.dart
+++ lib/request/apis/bangumi_api.dart
@@ -277,7 +277,7 @@ class BangumiApi {
             : [">=0", "<=99999"];
 
     final filter = <String, dynamic>{
-      "type": [2],
+      "type": [2,6],
       "tag": tags,
       "rank": rankFilter,
       "nsfw": false
